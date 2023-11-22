501759b06c9bf4ed62f7332887682f16176b443e
Gabriel <gabrielhenriquedps@gmail.com>
2023-11-22 02:51:52 -0300

    fix(empresa): Create form fixed

--- s3curity-app/src/pages/enterprises/components/createForm/index.tsx
+++ s3curity-app/src/pages/enterprises/components/createForm/index.tsx
@@ -54,53 +54,6 @@ export default function CreateForm() {
     setNumImageInputs(numImageInputs + 1);
   };
 
-  //Bloco de código refrente a criação de menus --------------------------------------->
-  const [numMenuInputs, setNumMenuInputs] = useState(1);
-  const [menus, setMenus] = useState<MenuData[]>([{ nomeMenu: '', empresa_id: 0, modulo_id: 0, itens: [] }]);
-
-  const addMenuInput = () => {
-    setNumMenuInputs(numMenuInputs + 1);
-  };
-
-  const addItemInput = (menuIndex: number) => {
-    const newMenus = [...menus];
-  
-    // Certifique-se de que newMenus[menuIndex] está definido
-    if (!newMenus[menuIndex]) {
-      newMenus[menuIndex] = {
-        nomeMenu: '', 
-        empresa_id: 0, 
-        modulo_id: 0, 
-        itens: [], // Inicialize como um array vazio
-      };
-    }
-  
-    // Certifique-se de que newMenus[menuIndex].itens está definido
-    if (!newMenus[menuIndex].itens) {
-      newMenus[menuIndex].itens = [];
-    }
-  
-    // Adicione um novo item ao array
-    newMenus[menuIndex].itens.push({
-      nomeItem: "",
-      relatorios: [],
-    });
-  
-    setMenus(newMenus);
-  };
-
-  const addRelatorioInput = (menuIndex: number, itemIndex: number) => {
-    const newMenus = [...menus];
-    newMenus[menuIndex].itens[itemIndex].relatorios.push({
-      nome: "",
-      relatorio: "",
-    });
-    setMenus(newMenus);
-  };
-
-
-   //FIm do bloco ------------------------------------------------------------------------>
-
   async function handleRegister(data: RegisterFormData) {
     console.log("entrou aq");
     try {
@@ -114,15 +67,6 @@ export default function CreateForm() {
         });
        // Assigning empresa_id to each menu in menuData
        console.log(response.data.empresa.id)
-    const menuData: MenuData[] = menus.map((menu) => ({
-      ...menu,
-      empresa_id: response.data.empresa.id,
-      modulo_id: 1,
-    }));
-
-    const menuResponses = await Promise.all(
-      menuData.map((menu) => api.post("menu/create", menu))
-    );
       back();
     } catch (e) {
       console.log(e);
@@ -206,98 +150,6 @@ export default function CreateForm() {
           Imagem +
         </button>
         </div>
-
-        {/* FORMS DOS MENUS -------------------------------------------------------> */}
-
-        <div>
-          {Array.from({ length: numMenuInputs }).map((_, menuIndex) => (
-            <div key={menuIndex} className={styles.menu}>
-              <div className={styles.inputWithContents}>
-                <input
-                  className={styles.inputForMenu}
-                  placeholder={`Nome do Menu ${menuIndex + 1}`}
-                  onChange={(e) => {
-                    const newMenus = [...menus];
-                    newMenus[menuIndex] = {
-                      ...newMenus[menuIndex],
-                      nomeMenu: e.target.value,
-                    };
-                    setMenus(newMenus);
-                  }}
-                />
-              </div>
-
-              {menus[menuIndex] && menus[menuIndex]?.itens && menus[menuIndex]?.itens.map((item, itemIndex) => (
-                <div key={itemIndex} className={styles.item}>
-                  <div className={styles.inputWithContents}>
-                    <input
-                      className={styles.inputForMenu}
-                      placeholder={`Nome do Item ${itemIndex + 1}`}
-                      onChange={(e) => {
-                        const newMenus = [...menus];
-                        newMenus[menuIndex].itens[itemIndex] = {
-                          ...newMenus[menuIndex].itens[itemIndex],
-                          nomeItem: e.target.value,
-                        };
-                        setMenus(newMenus);
-                      }}
-                    />
-                  </div>
-
-                  {item.relatorios.map((relatorio, relatorioIndex) => (
-                    <div key={relatorioIndex} className={styles.relatorio}>
-                      <div className={styles.inputWithContents}>
-                        <input
-                          className={styles.inputForMenu}
-                          placeholder={`Nome do Relatório ${relatorioIndex + 1}`}
-                          onChange={(e) => {
-                            const newMenus = [...menus];
-                            newMenus[menuIndex].itens[itemIndex].relatorios[relatorioIndex].nome = e.target.value;
-                            newMenus[menuIndex].itens[itemIndex].relatorios[relatorioIndex].relatorio = e.target.value;
-                            setMenus(newMenus);
-                          }}
-                        />
-                      </div>
-                      <div className={styles.inputWithContents}>
-                        <input
-                          className={styles.inputForMenu}
-                          placeholder={`Relatório ${relatorioIndex + 1}`}
-                          onChange={(e) => {
-                            const newMenus = [...menus];
-                            newMenus[menuIndex].itens[itemIndex].relatorios[relatorioIndex].relatorio = e.target.value;
-                            setMenus(newMenus);
-                          }}
-                        />
-                      </div>
-                    </div>
-                  ))}
-                  <button
-                    className={styles.addInput}
-                    type="button"
-                    onClick={() => addRelatorioInput(menuIndex, itemIndex)}
-                  >
-                    Relatório +
-                  </button>
-                </div>
-              ))}
-              <button
-                className={styles.addInput}
-                type="button"
-                onClick={() => addItemInput(menuIndex)}
-              >
-                Item +
-              </button>
-            </div>
-          ))}
-        </div>
-
-        <button
-          className={styles.addInput}
-          type="button"
-          onClick={addMenuInput}
-        >
-          Adicionar Menu
-        </button>
 
         <button className={styles.createUserButton} type="submit">
           Salvar
